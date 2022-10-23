2fa14840b4ee781d68cc9a4bbc4c04eecfd743c0
Ashley <ashleynjeri7@gmail.com>
2022-10-23 19:12:18 +0300

    passed tests

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    attr_accessor :breed
+
+    def initialize (name, breed='Mutt')
+        @name = name
+        @breed = breed
+    end
+
+end
+
+# TEST 
+#{
+    #sets the name of the dog in an instance variable @name
+    #sets the breed of the dog in an instance variable @breed
+    #defaults the breed argument to "Mutt" in an instance variable @breed
+
+#}
